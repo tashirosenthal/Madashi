01b83678635bf106a8faaa476bfbddc85816dcd4
tashirosenthal <tashirosenthal@gmail.com>
2017-06-07 12:47:28 -0700

    final project

--- FinalProject/quiz.java
+++ FinalProject/quiz.java
@@ -7,9 +7,8 @@ public class quiz
 		System.out.println("How are you feeling..." +
 		        "\n 1. I feel amazing and healthy." +
 				"\n 2. I feel like something is very wrong.");
-							  
 		int a = kb.nextInt();
-		if(a == 1)
+		 if(a == 1)
 		{
 			System.out.println("Sounds like you are cancer-free have a nice day!");
 		}
@@ -30,14 +29,13 @@ public class quiz
 								System.out.println("Okay Please add another description to your pain." +
 								"\n 1. Extreeme Abdominal Pain" +
 								"\n 2. Indigestion" +
-								"\n 3. Anemia or fever" +
-								"\n 4. Weight loss or Anorexia");
+								"\n 3. Anemia or fever");
 								int c = kb.nextInt();
 								if(c == 1)
 								{
 									System.out.println("Okay One more question which of these do your symptoms best identify with?" +
 									"\n 1. Fever and rash" + 
-									"\n 2. Anemia");
+									"\n 2. Dark Urine and pale feces");
 									int d = kb.nextInt();
 									if(d == 1)
 									{
@@ -45,12 +43,12 @@ public class quiz
 									}
 									else
 									{
-										System.out.println("You most likely have Cell Renal Cell Carcinoma, a cancer of the Kidney. Here is a link to more information regarding the disease.");
+										System.out.println("You most likely have Cholangiocarcinoma, a cancer of the bile duct. Here is a link to more information regarding the disease.");
 									}
 								}
 								else if(c == 2)
 								{
-									System.out.println("Okay One more question which of these do your symptoms best identify with?");
+									System.out.println("Okay One more question which of these do your symptoms best identify with?" +
 									"\n 1. Blood in Stool and constipation" + 
 									"\n 2. Bloating, Nausea and vomiting");
 									int e = kb.nextInt();
@@ -63,20 +61,45 @@ public class quiz
 										System.out.println("You most likely have GallBladder Carcinoma, a cancer of the GallBladder where bile is stored. Here is a link to more information regarding the disease.");
 									}
 								}
-							}
-							else if(r == 2)
+								else
+								{
+									System.out.println("Okay One more question which of these do your symptoms best identify with?" +
+									"\n 1. Weight loss nasuea and vomiting" +
+									"\n 2. Extreeme abdominal pain and constant fever");
+									int f = kb.nextInt();
+									if(f ==1)
+									{
+										System.out.println("You most likely have Ampullary Carcinoma, a cancer of the digestive tract. Here is a link to more information regarding the disease.");
+									}
+									else 
+									{
+										System.out.println("You most likely have Cell Renal Cell Carcinoma, a cancer of the kidney where blood is filtered. Here is a link to more information regarding the disease.");
+									}
+								}
+								}
+									
+							
+							else if(b == 2)
 							{
-								System.out.println("Would you like to" +
-								"\n 1.Take the shinkansen bullet train to hakkone?" +
-								"\n 2.Or take the subway to Roponggi?");
-								int h = kb.nextInt();
-								if(h == 1)
+								System.out.println("Okay One more question which of these do your symptoms best identify with?" +
+									"\n 1. Fever and rash" + 
+									"\n 2. Anemia");
+									int g = kb.nextInt();
+								    if(g == 1)
+									{
+										System.out.println("You most likely have Acinar Carcinoma, a cancer of the pancreas. Here is a link to more information regarding the disease.");
+									}
+									else
+									{
+										System.out.println("You most likely have Cell Renal Cell Carcinoma, a cancer of the Kidney. Here is a link to more information regarding the disease.");
+									}
+								
 								{
 									System.out.println("Sound relaxing! What would you like to do when you arrive?" +
 														"\n 1. Take a dip in the mountain spring spa?" +
 														"\n 2. Take a gondola over Mount Fuji?");
-														int n = kb.nextInt(); 
-														if(n ==1)
+														int h = kb.nextInt(); 
+														if(h ==1)
 														{
 															System.out.println("How refreshing have a nice stay!");
 														}
@@ -91,8 +114,8 @@ public class quiz
 									System.out.println("Excitemnt is in your near future! Do you want to" +
 														"\n 1.Eat at the famous restaurant from Kill Bill" +
 														"\n 2.Explore the manga world");
-														int v = kb.nextInt();
-														if(v == 1)
+														int i = kb.nextInt();
+														if(i == 1)
 														{
 															System.out.println("Yum! Make sure to get reservations it gets busy!");
 														}
@@ -101,7 +124,6 @@ public class quiz
 															System.out.println("Look for your favorite anime character and buy some suvoniers!");
 														}
 								}
-							}
+							}	}
 		}
 	}
-}
